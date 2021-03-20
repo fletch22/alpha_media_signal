57a806156cde70b6f34f6b2c98d7e4fd57742992
chris.flesche <chris@fletch22.com>
2021-03-19 19:29:43 -0500

    modifying deploy scripts

--- scripts/deploy/deploy_ams.cmd
+++ scripts/deploy/deploy_ams.cmd
@@ -1,1 +1,12 @@
-git clone https://github.com/fletch22/alpha_media_signal.git C:\Users\Chris\deploy_environments\prod\ams
+REM git clone https://github.com/fletch22/alpha_media_signal.git C:\Users\Chris\deploy_environments\prod\ams
+
+c:
+
+cd \Users\Chris\deploy_environments\prod\ams
+
+git checkout master
+
+REM reset this repository's master branch to the commit of interest
+git reset 44d4de6 --hard
+
+cd \Users\Chris\workspaces\alpha_media_signal\scripts\deploy\
