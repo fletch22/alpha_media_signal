c27b7e0657dacf97819f92cea5c60476d308ee7f
chris.flesche <chris@fletch22.com>
2021-03-19 19:40:37 -0500

    fixing deploy and start get tweets script;

--- scripts/deploy/deploy_ams.cmd
+++ scripts/deploy/deploy_ams.cmd
@@ -1,12 +1,16 @@
+SET COMMIT=57a8061
+
 REM git clone https://github.com/fletch22/alpha_media_signal.git C:\Users\Chris\deploy_environments\prod\ams
 
 c:
 
 cd \Users\Chris\deploy_environments\prod\ams
 
+git fetch --all
+
 git checkout master
 
 REM reset this repository's master branch to the commit of interest
-git reset 44d4de6 --hard
+git reset --hard %COMMIT%
 
 cd \Users\Chris\workspaces\alpha_media_signal\scripts\deploy\
--- scripts/get_tweets.cmd
+++ scripts/get_tweets.cmd
@@ -1,11 +1,11 @@
-SET PYTHONPATH="C:\Users\Chris\workspaces\alpha_media_signal"
-
 cd ..\ || exit
 
-REM call "activate.bat" alpha_media_signal
+SET PYTHONPATH=%CD%
+
+call "activate.bat" alpha_media_signal
 
 python -m ams.services.twitter_service
 
 cd .\scripts || exit
 
-REM call "conda deactivate"
+conda deactivate
