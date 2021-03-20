44d4de61f153e128d7e0cbd6057a52877a3a1059
chris.flesche <chris@fletch22.com>
2021-03-19 19:18:47 -0500

    modifying deploy scripts

--- scripts/deploy/deploy_ams.cmd
+++ scripts/deploy/deploy_ams.cmd
@@ -1,4 +1,1 @@
-git clone master C:\Users\Chris\deploy_environments\prod\ams
-
-
-C:\Users\Chris\deploy_environments\prod\ams\scripts\get_twitter.cmd
+git clone https://github.com/fletch22/alpha_media_signal.git C:\Users\Chris\deploy_environments\prod\ams
